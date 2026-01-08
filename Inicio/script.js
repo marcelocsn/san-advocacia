a247bdd1d67675ceef0be9c3b19aa7371160c04b
Marcelo Nicolela <csnicolela.marcelo@gmail.com>
2026-01-08 14:18:56 -0300

    Add files via upload

--- Inicio/script.js
+++ Inicio/script.js
@@ -101,3 +101,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const cookieBanner = document.getElementById("cookie-banner");
+  const acceptCookiesBtn = document.getElementById("accept-cookies");
+
+  if (!cookieBanner || !acceptCookiesBtn) return;
+
+  const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+
+  if (!cookiesAccepted) {
+    cookieBanner.style.display = "block";
+  }
+
+  acceptCookiesBtn.addEventListener("click", function () {
+    localStorage.setItem("cookiesAccepted", "true");
+    cookieBanner.style.display = "none";
+  });
+
+});
+
